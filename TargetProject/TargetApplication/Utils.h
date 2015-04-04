effb0baf0d3b1a1a3a722bf2ae0ce78d4bf7f8b6
furuhashi <test>
2015-04-05 03:58:43 +0900

    Utilsを追加

---
+++ TargetProject/TargetApplication/Utils.h
@@ -0,0 +1,14 @@
+#ifndef D_Utils_H
+#define D_Utils_H
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+#if 0
+	int FormatOutput(const char *, ...);
+#endif 
+
+	extern int(*FormatOutput)(const char *, ...);
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif  /* D_Utils_H */
